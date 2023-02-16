84acff8e792de2d6a5103f8ade89ba55308efc3f
nikhilbilla0070 <114826561+nikhilbilla0070@users.noreply.github.com>
2023-02-16 15:04:27 +0530

    add solution

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,23 @@ import React from "react";
 import { useState } from "react";
 
 const App = () => {
-  const [relativeList,setRealtiveList]=useState();
-  const [relativeListItem1,setRealtiveListItem1]=useState();
-
-  const [relativeListItem2,setRealtiveListItem2]=useState();
+  const relativeList = [
+    'Nikhil',
+    'Kittu',
+    'Raju',
+    'Sai'
+  ];
 
   return (
     <div id="main">
                {/* Do not remove the main div */}
-               <ol key={relativeList}>
-                 <li key={relativeListItem1}>Nikhil</li>
-                 <li key={relativeListItem2}>Nikki</li>
-               </ol>
+               <ol key="relativeList">
+      {relativeList.map((relative, index) => (
+        <li key={`relativeListItem1{index + 1}`}>
+          {relative}
+        </li>
+      ))}
+    </ol>
                
     </div>
   )
