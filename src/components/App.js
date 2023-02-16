430c6ddf0c2eae057d96b5a1e12ada6ebebc2661
nikhilbilla0070 <114826561+nikhilbilla0070@users.noreply.github.com>
2023-02-16 15:18:31 +0530

    add solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 
 const App = () => {
   const relativeList = [
@@ -11,8 +10,7 @@ const App = () => {
 
   return (
     <div id="main">
-               {/* Do not remove the main div */}
-               <ol key="relativeList">
+    <ol key="relativeList">
       <li key="relativeListItem1">{relativeList[0]}</li>
       <li key="relativeListItem2">{relativeList[1]}</li>
     </ol>          
